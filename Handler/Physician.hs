f598edcf8fc3d43bbdf4aaa7b0a6be1b3337cd33
Divam <dfordivam@gmail.com>
2016-05-15 19:47:27 +0530

    Fixed form text

--- Handler/Physician.hs
+++ Handler/Physician.hs
@@ -20,11 +20,8 @@ getAddPhysicianR = do
   defaultLayout
     [whamlet|
       <p>
-        The widget generated contains only the contents
-        of the form, not the form tag itself. So...
       <form method=post action=@{AddPhysicianR} enctype=#{enctype}>
         ^{widget}
-        <p>It also doesn't include the submit button.
         <button>Submit
     |]
 
@@ -38,7 +35,7 @@ postAddPhysicianR = do
       defaultLayout [whamlet|<p>#{show person}|]
     _ -> defaultLayout
       [whamlet|
-        <p>Invalid input, let's try again.
+        <p>Invalid input, please try again.
         <form method=post action=@{AddPhysicianR} enctype=#{enctype}>
           ^{widget}
           <button>Submit
