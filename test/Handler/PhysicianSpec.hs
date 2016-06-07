633375cc644575eec518afddf29976dddf3befdf
Divam <dfordivam@gmail.com>
2016-06-07 16:54:16 +0530

    Physician test: populate db and check for list

--- test/Handler/PhysicianSpec.hs
+++ test/Handler/PhysicianSpec.hs
@@ -6,6 +6,8 @@ import CustomDBDataTypes
 spec :: Spec
 spec = withApp $ do
   it "loads a form to add a new physician" $ do
+    runDB $ deleteWhere ([] :: [Filter Physician])
+
     get AddPhysicianR
     statusIs 200
     -- htmlAllContain "h1" "Add new physician information"
@@ -29,3 +31,48 @@ spec = withApp $ do
     (Entity _id obj:_) <- runDB $ selectList [PhysicianName ==. name] []
     assertEqual "Should have " obj (Physician name gender title Nothing)
 
+  it "displays a list of physicians which has link to detail page" $ do
+    runDB $ deleteWhere ([] :: [Filter Physician])
+    addPhysicians 25
+
+    get ListPhysicianR
+    statusIs 200
+
+    printBody
+
+    -- Has 10 in this page
+    bodyContains "Phys_1"
+    bodyContains "Phys_10"
+
+    -- Has link to 
+    -- Has a link to add new physician
+    bodyContains "Add new physician"
+
+    get $ ListPhysicianPageR 2
+    statusIs 200
+    bodyContains "Phys_11"
+    bodyContains "Phys_20"
+
+  it "displays a message when no physicians are present in database" $ do
+    get ListPhysicianR
+    statusIs 200
+
+    -- Has a link to add new physician
+    --
+  -- it "details page shows physician details, list of upcoming appointments\
+  --   \ past appointments, link to edit details" $ do
+  --   get ListPhysicianR
+  --   statusIs 200
+
+addPhysicians countN = do
+  unless (countN < 2) $ addPhysicians (countN - 1)
+
+  let physician = Physician name gender position remarks
+      name = pack $ "Phys_" ++ (show countN)
+      gender = if even countN then Male else Female
+      position = pack $ "Pos_" ++ (show countN)
+      remarks = Nothing
+      -- Just $ Textarea $ pack $ "Remarks for Phys_" ++ (show count)
+
+  runDB $ insert physician
+  return ()
