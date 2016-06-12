aeb3038b1bb7e072a96b6479353f2b87155e5f0a
Divam <dfordivam@gmail.com>
2016-06-12 19:43:54 +0530

    Add start time form in appointment creation. working

--- Handler/Appointment.hs
+++ Handler/Appointment.hs
@@ -4,10 +4,13 @@ import Import
 import CustomDBDataTypes
 import Handler.FormUtils
 
+appointmentStartTimeFormTag :: Text
+appointmentStartTimeFormTag = "appointmentStartTimeFormTag"
+
 appointmentForm :: Maybe Appointment ->
-  PatientId -> [(Text, PhysicianId)] -> [(Text, NurseId)]
+  PatientId -> [(Text, PhysicianId)] -> [(Text, NurseId)] -> UTCTime
   -> Form Appointment
-appointmentForm inp patId physicianList nurseList =
+appointmentForm inp patId physicianList nurseList startTime =
   renderDivs $ Appointment
   <$> pure patId
   <*> areq (selectFieldList physicianList) "Physician"
@@ -18,8 +21,7 @@ appointmentForm inp patId physicianList nurseList =
     (appointmentDiagnosis <$> inp)
   <*> areq textareaField "Comments"
     (appointmentComments <$> inp)
---  <*> areq textField "Start Time"
-  <*> lift (liftIO getCurrentTime)
+  <*> pure startTime
   --  (appointmentStartTime <$> inp)
   <*> pure Nothing
   -- <*> aopt textField "End Time"
@@ -32,17 +34,32 @@ getAddAppointmentR patId = do
   nurseList <- runDB $ selectList ([] :: [Filter Nurse])
     [Asc NurseName]
 
+  curTime <- lift (liftIO getCurrentTime)
   let physNameList = map 
         (\(Entity objId obj) -> (physicianName obj, objId)) physList
       nurseNameList = map
         (\(Entity objId obj) -> (nurseName obj, objId)) nurseList 
 
-  getAddRecordForm 
-    (appointmentForm Nothing patId physNameList nurseNameList)
-    (AddAppointmentR patId)
+  -- Generate the form to be displayed
+  (widget, enctype) <- generateFormPost
+    (appointmentForm Nothing patId physNameList nurseNameList curTime)
+
+  let addRoute = (AddAppointmentR patId)
+
+  defaultLayout $ do
+    aDomId <- newIdent
+    $(widgetFile "add-appointment")
 
 postAddAppointmentR :: PatientId -> Handler Html
 postAddAppointmentR patId = do
+  stime <- lookupPostParam appointmentStartTimeFormTag
+
+  curTime <- lift (liftIO getCurrentTime)
+
+  let startTime = fromMaybe curTime $
+        join (parseTime defaultTimeLocale "%FT%R" <$>
+          (unpack <$> stime))
+
   physList <- runDB $ selectList ([] :: [Filter Physician]) 
     [Asc PhysicianName]
   nurseList <- runDB $ selectList ([] :: [Filter Nurse])
@@ -53,7 +70,8 @@ postAddAppointmentR patId = do
       nurseNameList = map
         (\(Entity objId obj) -> (nurseName obj, objId)) nurseList 
 
-  postAddRecordForm Nothing (appointmentForm Nothing patId physNameList nurseNameList)
+  postAddRecordForm Nothing 
+    (appointmentForm Nothing patId physNameList nurseNameList startTime)
     (AddAppointmentR patId)
 
 getListAppointmentR :: Handler Html
