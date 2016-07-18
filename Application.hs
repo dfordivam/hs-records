d5fa3f523d8f496a04d0a501dc14d4e135157551
Divam <dfordivam@gmail.com>
2016-07-18 12:33:42 +0530

    Removed comment functionality

--- Application.hs
+++ Application.hs
@@ -33,7 +33,6 @@ import System.Log.FastLogger                (defaultBufSize, newStdoutLoggerSet,
 -- Don't forget to add new modules to your cabal file!
 import Handler.Common
 import Handler.Home
-import Handler.Comment
 import Handler.Physician
 import Handler.Patient
 import Handler.Appointment
