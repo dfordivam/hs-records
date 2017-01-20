4bfb763bcc3fc04d8c5019dcb0e719829c526f4a
Divam <dfordivam@gmail.com>
2017-01-20 22:58:41 +0530

    Slight re-write of PhysicianSpec

--- test/Handler/PhysicianSpec.hs
+++ test/Handler/PhysicianSpec.hs
@@ -3,123 +3,128 @@ module Handler.PhysicianSpec (spec) where
 import TestImport
 import CustomDBDataTypes
 import Handler.TestUtils
+import Yesod.Form.Fields (unTextarea)
 
 spec :: Spec
 spec = withApp $ do
-  it "add a new physician" $ do
-    runDB $ deleteWhere ([] :: [Filter Physician])
+  describe "Physician Handler" $ do
+    describe "Basic CRUD operations" $ do
+      it "Creates/adds a new physician entry" $ do
+        runDB $ deleteWhere ([] :: [Filter Physician])
 
-    withFullUserLogin ( do
-    get AddPhysicianR
-    statusIs 403
-    )
+        withFullUserLogin ( do
+        get AddPhysicianR
+        statusIs 403
+        )
 
-    withAdminLogin ( do
-    get AddPhysicianR
-    statusIs 200
-    -- htmlAllContain "h1" "Add new physician information"
+        withAdminLogin ( do
+        get AddPhysicianR
+        statusIs 200
+        -- htmlAllContain "h1" "Add new physician information"
 
-    let name = "Physician's Name"
-        title = "Physician's Title"
-        gender = Female
+        let name = "Physician's Name"
+            title = "Physician's Title"
+            gender = Female
 
-    request $ do
-      setMethod "POST"
-      setUrl AddPhysicianR
-      addToken
+        request $ do
+          setMethod "POST"
+          setUrl AddPhysicianR
+          addToken
 
-      byLabel "Name" name
-      byLabel "Gender" "1"
-      byLabel "Position" title
-      byLabel "Active" "1"
+          byLabel "Name" name
+          byLabel "Gender" "1"
+          byLabel "Position" title
+          byLabel "Active" "1"
 
-    -- printBody
-    statusIs 200
+        -- printBody
+        statusIs 200
 
-    (Entity _ obj:_) <- runDB $ selectList [PhysicianName ==. name] []
-    assertEqual "Should have " obj (Physician name gender title Nothing False)
-    )
+        (Entity _ obj:_) <- runDB $ selectList [PhysicianName ==. name] []
+        assertEqual "Should have " obj (Physician name gender title Nothing False)
+        )
 
-  it "displays a list of physicians which has link to detail page" $ do
-    runDB $ deleteWhere ([] :: [Filter Physician])
-    addPhysicians 25
+      it "edits/updates the record" $ do
+        addPhysicians 25
+        Just (Entity objId obj) <- runDB $ selectFirst ([] :: [Filter Physician]) []
 
-    withRestrictedUserLogin ( do
-    get ListPhysicianR
-    statusIs 403
-    )
-    -- This require Full priviledges
+        withFullUserLogin ( do
+        get $ EditPhysicianR objId
+        statusIs 403
+        )
 
-    withFullUserLogin ( do
-    get ListPhysicianR
-    followRedirect
-    statusIs 200
+        withAdminLogin ( do
+        get $ EditPhysicianR objId
+        statusIs 200
 
-    -- Has 10 in this page
-    bodyContains "Phys_1"
-    bodyContains "Phys_10"
+        let oldName = physicianName obj
+            oldRemarks = physicianRemarks obj
 
-    bodyContains "Add new physician"
+        bodyContains (unpack oldName)
 
-    -- Check 2nd page
-    get $ ListPhysicianPageR 2
-    statusIs 200
-    bodyContains "Phys_20"
+        let newName = oldName ++ "_newname"
+            newRemarks = "some new remark" :: Text
 
-    bodyContains "Add new physician"
-    )
+        request $ do
+          setMethod "POST"
+          setUrl $ EditPhysicianR objId
+          addToken
 
-  it "edit the record" $ do
-    addPhysicians 25
-    Just (Entity objId obj) <- runDB $ selectFirst ([] :: [Filter Physician]) []
+          byLabel "Name" newName
+          byLabel "Gender" "1"
+          byLabel "Position" (physicianPosition obj)
+          byLabel "Remarks" newRemarks
+          byLabel "Active" "1"
 
-    withFullUserLogin ( do
-    get $ EditPhysicianR objId
-    statusIs 403
-    )
+        statusIs 200
 
-    withAdminLogin ( do
-    get $ EditPhysicianR objId
-    statusIs 200
+        Just (Entity _ newObj) <- runDB $  selectFirst ([] :: [Filter Physician]) []
+        --getBy $ objId
 
-    let oldName = physicianName obj
-        oldRemarks = physicianRemarks obj
+        assertEqual "Name should be" (physicianName newObj) (newName)
+        assertEqual "Position should be" (physicianPosition newObj) (physicianPosition obj)
+        assertEqual ("Remarks should be " ++ (show $ physicianRemarks newObj)) (fmap unTextarea (physicianRemarks newObj)) (Just newRemarks)
+        )
 
-    bodyContains (unpack oldName)
+      -- it "details page shows physician details, list of upcoming appointments\
+      --   \ past appointments, link to edit details" $ do
+      --   get ListPhysicianR
+      --   statusIs 200
 
-    let newName = oldName ++ "_newname"
-        newRemarks = "some new remark" :: Text
+    describe "Pagination" $ do
+      it "displays a list of physicians which has link to detail page" $ do
+        runDB $ deleteWhere ([] :: [Filter Physician])
+        addPhysicians 25
 
-    request $ do
-      setMethod "POST"
-      setUrl $ EditPhysicianR objId
-      addToken
+        withRestrictedUserLogin ( do
+        get ListPhysicianR
+        statusIs 403
+        )
+        -- This require Full priviledges
 
-      byLabel "Name" newName
-      byLabel "Gender" "1"
-      byLabel "Position" (physicianPosition obj)
-      byLabel "Remarks" newRemarks
-      byLabel "Active" "1"
+        withFullUserLogin ( do
+        get ListPhysicianR
+        followRedirect
+        statusIs 200
 
-    statusIs 200
+        -- Has 10 in this page
+        bodyContains "Phys_1"
+        bodyContains "Phys_10"
 
-    Just (Entity _ newObj) <- runDB $  selectFirst ([] :: [Filter Physician]) []
-    --getBy $ objId
+        bodyContains "Add new physician"
 
-    assertEqual "Name should be" (physicianName newObj) (newName)
-    assertEqual "Position should be" (physicianPosition newObj) (physicianPosition obj)
-    -- assertEqual "Remarks should be" (physicianRemarks newObj) (Just newRemarks)
-    )
+        -- Check 2nd page
+        get $ ListPhysicianPageR 2
+        statusIs 200
+        bodyContains "Phys_20"
 
-  -- it "displays a message when no physicians are present in database" $ do
-  --   get ListPhysicianR
-  --   followRedirect
-  --   statusIs 200
+        bodyContains "Add new physician"
+        )
 
-  --   -- Has a link to add new physician
-    --
-  -- it "details page shows physician details, list of upcoming appointments\
-  --   \ past appointments, link to edit details" $ do
-  --   get ListPhysicianR
-  --   statusIs 200
 
+      -- it "displays a message when no physicians are present in database" $ do
+      --   get ListPhysicianR
+      --   followRedirect
+      --   statusIs 200
+
+      --   -- Has a link to add new physician
+        --
